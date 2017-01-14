e470a0368dd8d5e8cb5ad8cba68c2785bfc9442b
cookiej91 <jsc91@live.co.uk>
2017-01-14 09:32:20 +0000

    Roll and checkletters works
    roll() now rolls randomly through the array.

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var letterPicked = "";
+var rollClick = document.getElementById('rollClick');
+
 var letters = [
   "A", "B", "C", "D", "E", "F",
   "G", "H", "I", "J", "K", "L",
@@ -12,10 +15,29 @@ var usedLetters = [
 
 function checkLetters(char){
   if (usedLetters.includes(char)) {
-    reroll();
+    roll();
   } else {
-    usedLetters.push(char);
-    console.log(char);
-
+    usedLetters.push(letterPicked);
   }
+}
+
+function roll(){
+  if(usedLetters.length > 25){
+    alert("Finished");
+    reset();
+  }
+  letterPicked = letters[Math.floor(Math.random() * letters.length)];
+  checkLetters(letterPicked);
+}
+
+rollClick.addEventListener("click", function(event) {
+  roll();
+  console.log(letterPicked);
+  rollClick.value = "hello";
+}, false);
+
+function reset() {
+  usedLetters = [
+    
+  ]
 }
